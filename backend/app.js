92042c20886be024b2d3054718186f0008e39045
kahole <kristian@hole.priv.no>
2018-10-01 11:00:17 +0200

    adds spotify search

--- backend/app.js
+++ backend/app.js
@@ -2,16 +2,33 @@ const express = require('express');
 const app = express();
 const jsonBodyParser = require('body-parser').json();
 const ip = require('ip');
+const SpotifyApi = require('spotify-web-api-node');
 
 const PLAYING = 0;
 const PAUSED = 1;
 
+const spotifyApi = new SpotifyApi({
+    clientId: '9f3d6b92343b4790ac71380e072160cb',
+    clientSecret: 'bdca98b1024b462a9a0a8f64f48efe5c'/*,
+    redirectUri: 'http://www.example.com/callback'*/
+});
+
+spotifyApi.clientCredentialsGrant().then(
+    function (data) {
+        console.log('The access token is ' + data.body['access_token']);
+        spotifyApi.setAccessToken(data.body['access_token']);
+    },
+    function (err) {
+        console.log('Something went wrong!', err);
+    }
+);
+
 app.use(jsonBodyParser);
 
-var youtube = {queue: [], playingIndex: 0, playerState: PLAYING};
+var youtube = { queue: [], playingIndex: 0, playerState: PLAYING };
 
 function idle() {
-    return (youtube.queue.length == 0 || youtube.playingIndex > youtube.queue.length-1);
+    return (youtube.queue.length == 0 || youtube.playingIndex > youtube.queue.length - 1);
 }
 
 // app.use(express.static('/home/pi/hvemcasta/public'));
@@ -22,7 +39,7 @@ app.use('/search', express.static('public'));
 app.use('/cast', express.static('public'));
 
 app.get('/yt/removeFromQueue/:id', (req, res) => {
-    youtube.queue = youtube.queue.filter( v => v.id !== req.params.id);
+    youtube.queue = youtube.queue.filter(v => v.id !== req.params.id);
     res.send();
 });
 
@@ -33,7 +50,7 @@ app.post('/yt/queue', (req, res) => {
 
 app.get('/status', (req, res) => {
     let ipAddress = process.env.PORT === 80 ? ip.address() : ip.address() + ":" + process.env.PORT;
-    res.send({idle: idle(), ip: ipAddress });
+    res.send({ idle: idle(), ip: ipAddress });
 });
 
 app.get('/yt', (req, res) => {
@@ -66,6 +83,17 @@ app.get('/yt/pause', (req, res) => {
 app.get('/yt/play', (req, res) => {
     youtube.playerState = PLAYING;
     res.send();
+});
+
+app.get('/spotify/search', (req, res) => {
+    spotifyApi.searchTracks(req.query.query, { limit: 45 })
+        .then(function (data) {
+            console.log(data.body);
+            res.send({ tracks: data.body.tracks.items });
+        }, function (err) {
+            console.error(err);
+            res.send({err});
+        });
 });
 
 app.listen(process.env.PORT, () => console.log('Cast server started, port ' + process.env.PORT));
--- client/src/Router.js
+++ client/src/Router.js
@@ -1,7 +1,7 @@
 import SimpleReactRouter from 'simple-react-router';
  
 import Cast from './cast/Cast.js';
-import Search from './controller/Search.js';
+import Search from './search/Search.js';
 import Controller from './controller/Controller.js';
  
 export default class Router extends SimpleReactRouter {
